cd26149ffaba52f6af1de6d869e54c2e77933668
Tristan Partridge <tristanp2@hotmail.com>
2016-04-03 23:26:07 -0700

    fixed typo that made back layer scroll too slowly

--- background.h
+++ background.h
@@ -43,8 +43,8 @@ public:
         delta_back.y = offset_y/2 - last_py;
         front_rect.x = -front_rect.w - delta_front.x;
         front_rect.y = -front_rect.h - delta_front.y;
-        para_rect.x = -para_rect.w - delta_back.x/2;
-        para_rect.y = -para_rect.h - delta_back.y/2;
+        para_rect.x = -para_rect.w - delta_back.x;
+        para_rect.y = -para_rect.h - delta_back.y;
 
         if(front_rect.x > 0 or front_rect.x < -2*front_rect.w){
             front_rect.x = -front_rect.w + front_rect.x % front_rect.w;
@@ -93,7 +93,7 @@ private:
     int tile_x, tile_y;
 
     void generate_background(){
-        starfield = new StarField(r, pixel_format, front_rect.w, front_rect.h, false, front_rect.w*2, front_rect.w);
+        starfield = new StarField(r, pixel_format, front_rect.w, front_rect.h);
         parallax = new StarField(r, pixel_format, para_rect.w, para_rect.h, true, 200, 600);
     }
 };
